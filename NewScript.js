59841896d570327e97c6a0d50c80fb9bf86f81c7
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-12-11 08:39:56 +0100

    Remove errorMessageVisible

--- NewScript.js
+++ NewScript.js
@@ -7,22 +7,20 @@ let btn = document.getElementById("btn")
 
 passwordContainer[0].appendChild(errorMessage);
 
-errorMessage.className = "errorMessage"
-
 
 btn.addEventListener('click', function () {
     let passwordValue = password.value;
     let confirmPasswordValue =confirmPassword.value;
-    console.log(passwordValue.length)
+
 
     if ( passwordValue !== confirmPasswordValue ) {
-        errorMessage.className = "errorMessageVisible"
+        errorMessage.className = "errorMessage"
         errorMessage.textContent = "* Password does not match"
     }
 
         
     else if ( passwordValue.length < 7 || passwordValue.length > 12){
-        errorMessage.className = "errorMessageVisible"
+        errorMessage.className = "errorMessage"
         errorMessage.textContent = "* Mix alphabets (not case-sensitive), and numbers.Total 7~12 words"
         console.log("Hello World")
         console.log("Good Night")
